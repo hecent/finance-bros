2430b14ed727e7b9b73439c9034875a0ca8587de
Lachlan Horsfield <lh277@st-andrews.ac.uk>
2026-03-14 12:57:07 +0000

    adding passive events

---
+++ src/backend/model/PassiveEvent.py
@@ -0,0 +1,7 @@
+from model.Effect import Effect
+
+class PassiveEvent:
+    def __init__(self, description, message, effect):
+        self.id = event_id
+        self.message = message
+        self.effect = effect
