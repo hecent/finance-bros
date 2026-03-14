05e97b70b9f0f59e47d3db35a53cd265d6398898
nkhetarpal906 <nkhetarpal906@gmail.com>
2026-03-14 14:00:12 +0000

    creating and adding passive events

--- src/backend/model/PassiveEvent.py
+++ src/backend/model/PassiveEvent.py
@@ -1,7 +1,8 @@
 from model.Effect import Effect
 
 class PassiveEvent:
-    def __init__(self, description, message, effect):
+    def __init__(self, event_id, message, effect, probability=1.0):
         self.id = event_id
         self.message = message
         self.effect = effect
+        self.probability = probability
